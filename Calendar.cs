d97a006905cc9c3ca2dd3609a8456199c963a186
PistaMista-Laptop <krystofalbrechtus@gmail.com>
2018-09-02 14:56:35 +0200

    Finished the calendar.

--- Calendar.cs
+++ Calendar.cs
@@ -1,63 +1,141 @@
 using System;
+using System.Linq;
 
 namespace Atlas
 {
     static class Calendar
     {
-        enum TimelineLayer
+        enum Layer
         {
-            NONE,
             YEAR,
             MONTH,
             DAY
         }
-        private static TimelineLayer currentLayer = TimelineLayer.NONE;
-        private static TimelineLayer CurrentLayer
-        {
-            get => currentLayer;
-            set
-            {
-                if (currentLayer != value)
-                {
-                    LoadItems(value, currentLayer == TimelineLayer.NONE ? DateTime.Today.Date : items[position]);
-                    currentLayer = value;
-                }
-            }
-        }
-        private static DateTime[] items;
-        private static int position;
-        public static int Position
-        {
-            get => position;
-            set
-            {
-                if (value >= 0 && value < items.Length)
-                {
-                    position = value;
-                    Show(CurrentLayer);
-                }
-            }
-        }
-
-        static void LoadItems(TimelineLayer layer, DateTime source)
-        {
-            items = new DateTime[layer == TimelineLayer.YEAR ? 1000 : (layer == TimelineLayer.MONTH ? 12 : DateTime.DaysInMonth(source.Year, source.Month))];
-        }
-
+        static Layer browsing;
+        static DateTime[] options;
+        static DateTime date;
         public static void Launch()
         {
-            CurrentLayer = TimelineLayer.YEAR;
+            date = DateTime.Today.Date;
+            browsing = Layer.DAY;
+            RefreshOptions();
+            UpdateDisplay();
 
             while (true)
             {
+                int vertical;
+                int horizontal;
+                while (!Program.ReadArrowKeys(out vertical, out horizontal)) Console.Beep();
+                Tuple<int, int, int> position = date.GetPositionData();
 
+                if (vertical != 0)
+                {
+                    int new_position = position.Item2 - vertical;
+                    if (new_position >= position.Item1 && new_position <= position.Item3) date = date.SetPosition(new_position);
+                }
+                else
+                {
+                    int new_layer = (int)browsing + horizontal;
+                    if (new_layer > 2)
+                    {
+                        break;
+                    }
+                    else if (new_layer < 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        browsing = (Layer)new_layer;
+                        RefreshOptions();
+                    }
+                }
+
+                UpdateDisplay();
+            }
+        }
+        static void RefreshOptions()
+        {
+            options = new DateTime[browsing == Layer.YEAR ? 100 : (browsing == Layer.MONTH ? 12 : DateTime.DaysInMonth(date.Year, date.Month))];
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                switch (browsing)
+                {
+                    case Layer.YEAR:
+                        options[i] = new DateTime(DateTime.Today.Date.Year + i, 1, 1);
+                        break;
+                    case Layer.MONTH:
+                        options[i] = new DateTime(date.Year, i + 1, 1);
+                        break;
+                    case Layer.DAY:
+                        options[i] = new DateTime(date.Year, date.Month, i + 1);
+                        break;
+                }
+            }
+        }
+        static void UpdateDisplay()
+        {
+            int space = Console.WindowHeight;
+            int currentPosition = browsing == Layer.YEAR ? date.Year - options.First().Year : (browsing == Layer.MONTH ? date.Month - 1 : date.Day - 1);
+            Console.Clear();
+
+            for (int i = currentPosition - space / 2; i < currentPosition + space / 2; i++)
+            {
+                if (i >= 0 && i < options.Length)
+                {
+                    if (i == currentPosition)
+                    {
+                        ConsoleColor prevColor = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(options[i].ToLongDateString());
+                        Console.ForegroundColor = prevColor;
+                    }
+                    else Console.WriteLine(options[i].GetMenuString());
+                }
+                else
+                {
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+        static string GetMenuString(this DateTime x)
+        {
+            switch (browsing)
+            {
+                case Layer.YEAR:
+                    return x.Year.ToString();
+                case Layer.MONTH:
+                    return x.Month.ToString() + " " + x.Year.ToString();
+                case Layer.DAY:
+                    return x.Day.ToString() + ". " + x.DayOfWeek.ToString();
             }
 
-            currentLayer = TimelineLayer.NONE;
+            return x.ToLongDateString();
         }
-        static void Show(TimelineLayer layer)
-        {
 
+        static Tuple<int, int, int> GetPositionData(this DateTime x)
+        {
+            return new Tuple<int, int, int>
+            (browsing == Layer.YEAR ? options.First().Year : 1,
+            browsing == Layer.YEAR ? x.Year : (browsing == Layer.MONTH ? x.Month : x.Day),
+            browsing == Layer.YEAR ? options.Last().Year : (browsing == Layer.MONTH ? 12 : DateTime.DaysInMonth(date.Year, date.Month)));
+        }
+
+        static DateTime SetPosition(this DateTime x, int position)
+        {
+            switch (browsing)
+            {
+                case Layer.YEAR:
+                    return new DateTime(position, 1, 1);
+                case Layer.MONTH:
+                    return new DateTime(x.Year, position, 1);
+                case Layer.DAY:
+                    return new DateTime(x.Year, x.Month, position);
+            }
+
+            return new DateTime();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -40,5 +40,31 @@ namespace Atlas
             Console.Write(s);
             Console.ForegroundColor = start_color;
         }
+
+        public static bool ReadArrowKeys(out int vertical, out int horizontal)
+        {
+            horizontal = 0;
+            vertical = 0;
+            ConsoleKeyInfo keyinfo = Console.ReadKey();
+            switch (keyinfo.Key.ToString())
+            {
+                case "UpArrow":
+                    vertical = 1;
+                    break;
+                case "DownArrow":
+                    vertical = -1;
+                    break;
+                case "RightArrow":
+                    horizontal = 1;
+                    break;
+                case "LeftArrow":
+                    horizontal = -1;
+                    break;
+                default:
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
